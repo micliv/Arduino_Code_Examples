3b842c627e17ce27026c3bb1579ddf1cea2e0359
Michael Liva <1717212+micliv@users.noreply.github.com>
2018-10-19 07:20:14 -0500

    Add files via upload

---
+++ BeckerSign/BeckerSignTest1/BeckerSignTest1.ino
@@ -0,0 +1,41 @@
+#include <Adafruit_NeoPixel.h>
+const int RGB = 4;
+int i = 0;
+int j = 0;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, RGB);
+void setup() 
+{
+strip.begin();
+strip.show();
+Serial.begin(9600);
+}
+
+void loop ()
+{
+for(i=0; i<10;i++) 
+{
+strip.setPixelColor(i,0,255,0);
+strip.show();
+delay(1000);
+}
+for(i=0; i<10;i++) 
+{
+strip.setPixelColor(i,0,0,0);
+strip.show();
+delay(1000);
+}
+
+
+for(i=0; i<10;i++) 
+{
+strip.setPixelColor(i,255,0,0);
+strip.show();
+delay(1000);
+}
+for(i=0; i<10;i++) 
+{
+strip.setPixelColor(i,0,0,255);
+strip.show();
+delay(1000);
+}
+}
